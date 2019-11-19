68d080f3056ffa47cc64c4a52ab0a1fca3a07d6d
YSHsieh7777 <leo67280@gmail.com>
2019-11-19 19:32:31 +0800

    Add init map and person.

--- project/person.cpp
+++ project/person.cpp
@@ -1,6 +1,83 @@
 #include "person.hpp"
 
-Person::Person(float2 init_pos, float2 init_dir) : position(init_pos), moving_direction(init_dir)
-{
+std::map<uint8_t, uint8_t> panic_to_red = {{0, 0}, {1, 255}};
+std::map<uint8_t, uint8_t> panic_to_green = {{0, 0}, {1, 0}};
+std::map<uint8_t, uint8_t> panic_to_blue = {{0, 0}, {1, 0}};
 
+Person::Person(float x, float y , float r)
+{
+    m_position = std::make_pair(x, y);
+    m_radius = r;
+    m_moving_direction = std::make_pair(0, 0);
+
+    m_panic_degree = 0;
+	m_moving_distance_last_few_seconds = 0;  // A factor that affects panic_degree.
+    m_dead = false;  
+}
+
+void Person::move()
+{
+    //Move the Person left or right
+    m_position.first += m_moving_direction.first;
+
+    //If the Person collided or went too far to the left or right
+	if((m_position.first-m_radius < 0) || (m_position.first+m_radius > SCREEN_WIDTH))
+    {
+        m_position.first -= m_moving_direction.first;
+    }
+
+    //Move the Person up or down
+    m_position.second += m_moving_direction.second;
+
+    //If the Person collided or went too far up or down
+    if((m_position.second-m_radius < 0) || (m_position.second+m_radius > SCREEN_HEIGHT))
+    {
+        //Move back
+        m_position.second -= m_moving_direction.second;
+    }
+}
+
+void Person::setColor()
+{
+    SDL_SetRenderDrawColor(gRenderer, panic_to_red[m_panic_degree], panic_to_green[m_panic_degree], panic_to_blue[m_panic_degree], 0xFF);
+}
+
+void Person::render()
+{
+    Person::setColor();
+
+    const float diameter = (m_radius * 2);
+
+    float x = (m_radius - 1);
+    float y = 0;
+    float tx = 1;
+    float ty = 1;
+    float error = (tx - diameter);
+
+    while (x >= y)
+    {
+        //  Each of the following renders an octant of the circle
+        SDL_RenderDrawPoint(gRenderer, m_position.first + x, m_position.second - y);
+        SDL_RenderDrawPoint(gRenderer, m_position.first + x, m_position.second + y);
+        SDL_RenderDrawPoint(gRenderer, m_position.first - x, m_position.second - y);
+        SDL_RenderDrawPoint(gRenderer, m_position.first - x, m_position.second + y);
+        SDL_RenderDrawPoint(gRenderer, m_position.first + y, m_position.second - x);
+        SDL_RenderDrawPoint(gRenderer, m_position.first + y, m_position.second + x);
+        SDL_RenderDrawPoint(gRenderer, m_position.first - y, m_position.second - x);
+        SDL_RenderDrawPoint(gRenderer, m_position.first - y, m_position.second + x);
+
+        if (error <= 0)
+        {
+            ++y;
+            error += ty;
+            ty += 2;
+        }
+
+        if (error > 0)
+        {
+            --x;
+            tx += 2;
+            error += (tx - diameter);
+        }
+    }
 }
--- project/person.hpp
+++ project/person.hpp
@@ -1,16 +1,38 @@
-#include <iostream>
+#include <utility>
 #include <stdint.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <map>
+#include <vector>
 
-#include "linalg.h"
+typedef std::pair<float, float> pos_t; 
+typedef std::pair<float, float> vel_t; 
 
-using float2 = linalg::aliases::float2;
+const int SCREEN_WIDTH = 590;
+const int SCREEN_HEIGHT = 590;
+
+extern SDL_Renderer* gRenderer;
+extern SDL_Window* gWindow;
 
 class Person
 {
-private:   
-    int16_t panic_degree;
-    float2 position;
-    float2 moving_direction;
-    float moving_distance_last_few_seconds;  // A factor that affects panic_degree.
-    bool dead;  // dead or not
-}
+public:
+	Person(float x, float y , float r);
+
+	//Moves the Person and checks collision
+	void move();
+
+	void setColor();
+
+	//Shows the Person on the screen
+	void render();
+
+private:
+	pos_t m_position;
+	float m_radius;
+	vel_t m_moving_direction;
+
+	uint8_t m_panic_degree;
+	float m_moving_distance_last_few_seconds;  // A factor that affects panic_degree.
+    bool m_dead;
+};
